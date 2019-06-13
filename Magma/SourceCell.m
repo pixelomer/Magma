fc25e0adeef88bb3039c7f32fcbe9c9f88ac62f4
pixelomer <37103802+pixelomer@users.noreply.github.com>
2019-06-13 17:12:19 +0300

    This is not what I thought it was

--- Magma/SourceCell.m
+++ Magma/SourceCell.m
@@ -7,7 +7,7 @@
 - (instancetype)initWithReuseIdentifier:(NSString *)reuseIdentifier {
 	self = [super initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:reuseIdentifier];
 	self.imageView.image = [[UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"file:///Applications/Cydia.app/Icon-60.png"]]] resizedImageOfSize:CGSizeMake(40, 40)];
-	self.separatorInset = UIEdgeInsetsMake(0, 20, 0, 20);
+	self.separatorInset = UIEdgeInsetsZero;
 	self.imageView.layer.masksToBounds = YES;
 	self.imageView.layer.cornerRadius = 10.0;
 	self.textLabel.text = @"(Unknown)";
